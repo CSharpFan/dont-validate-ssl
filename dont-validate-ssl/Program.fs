27a2a778d75ad155aab4696b889158cde160e398
Kristof Mattei <github@kristofmattei.be>
2016-06-09 16:22:07 +0200

    Added the code

--- dont-validate-ssl/Program.fs
+++ dont-validate-ssl/Program.fs
@@ -1,7 +1,38 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open System.Net
+open System.Net.Security
+
+type Result =
+| Ok of string
+| Error of exn
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main _ = 
+    
+    // option 1
+    // ServicePointManager.ServerCertificateValidationCallback <- new RemoteCertificateValidationCallback(fun _ _ _ _ -> true)
+
+    let result = 
+        try
+            let request = 
+                "https://somewebsite/with/expired/ssl/certificate/data.json?paramx=1&paramy=2"
+                |> WebRequest.CreateHttp
+
+            // option 2
+            // request.ServerCertificateValidationCallback <- new RemoteCertificateValidationCallback(fun _ _ _ _ -> true)
+
+            let response = 
+                request.GetResponse ()
+
+            // parse data
+            let parsed = "..." 
+
+            Ok parsed
+        with
+        | ex ->      
+            Error ex
+
+    // process result
+    // ...
+
+    0 
